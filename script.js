940753e242c34814bc42ad948a33153c017ad880
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-07-10 22:53:39 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,67 +2,85 @@ let techkaro = {
     name: "Tech Karo",
     about: "A Web Dev Course",
     description: "This program is held for womens.atfer this program every women and girl will change her life every girl can do every thing.Now female loves tech.",
-    students: [
-   {
-        name: "Ammara",
-        age: 19,
-        email: "ammarazahid845@gmail.com",
-        group: "B",
-        gender: "Female",
-        address: "Korangi,karachi",
-        contact: "03408589699",
-    },
-    {
-        name: "Abeera",
-        age: 20,
-        email: "abeera2900@gmail.com",
-        group: "B",
-        gender: "Female",
-        address: "karachi",
-        contact: "0346807587",    
-    },
-    {
-        name: "Ramsha",
-        age: 18,
-        email: "Ramsha567@gmail.com",
-        group: "B",
-        gender: "Female",
-        address: "Garden,karachi",
-        contact: "05359724444",
-    },
-    {
-        name: "Areeba",
-        age: 20,
-        email: "areebasaif980@gmail.com",
-        group: "B",
-        gender: "Female",
-        address: "Garden,karachi",
-        contact: "0346788889",
-    },
-    {
-        name: "Tooba",
-        age: 15,
-        email: "tooba670@gmail.com",
-        group: "B",
-        gender: "Female",
-        address: "Malir,karachi",
-        contact: "021-45687799",
-    },
-    {
-        name: "Ayesha",
-        age: 20,
-        email: "ayesha777@gmail.com",
-        group: "B",
-        gender: "Female",
-        address: "Malir,karachi",
-        contact: "03456666699",
-    }
-]
+    students: [{
+            name: "Ammara",
+            age: 19,
+            email: "ammarazahid845@gmail.com",
+            group: "B",
+            gender: "Female",
+            address: "Korangi,karachi",
+            contact: "03408589699",
+        },
+        {
+            name: "Abeera",
+            age: 20,
+            email: "abeera2900@gmail.com",
+            group: "B",
+            gender: "Female",
+            address: "karachi",
+            contact: "0346807587",
+        },
+        {
+            name: "Ramsha",
+            age: 18,
+            email: "Ramsha567@gmail.com",
+            group: "B",
+            gender: "Female",
+            address: "Garden,karachi",
+            contact: "05359724444",
+        },
+        {
+            name: "Areeba",
+            age: 20,
+            email: "areebasaif980@gmail.com",
+            group: "B",
+            gender: "Female",
+            address: "Garden,karachi",
+            contact: "0346788889",
+        },
+        {
+            name: "Tooba",
+            age: 15,
+            email: "tooba670@gmail.com",
+            group: "B",
+            gender: "Female",
+            address: "Malir,karachi",
+            contact: "021-45687799",
+        },
+        {
+            name: "Ayesha",
+            age: 20,
+            email: "ayesha777@gmail.com",
+            group: "B",
+            gender: "Female",
+            address: "Malir,karachi",
+            contact: "03456666699",
+        }
+    ]
+}
+
 
 
 let whatsapp = {
-     
+    appName: "whatsApp",
+    about: "for contact and fun",
+    description: "In this we can make a call,video call, we can send messages,we can upload pictures and status as well.",
+    howToUse: "For using this we need wifi connection or data connection",
+    contacts: [{
+            name: "Miss Abeera",
+            lastmessage: "Kal bt karen??",
+            time: "9:30 PM",
+            contact: "0340345698",
+            profilePic: "image",
+        },
+        {
+            name: "Miss Khair-un-nisa",
+            lastmessage: "Miss",
+            time: "3:56 PM",
+            contact: "0322222298",
+            profilePic: "image",
+        },
 
 
-
+    ]
 }
