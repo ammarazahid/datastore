5c735120822c5a09926c8807a769d2ed5fb1f07e
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-07-13 02:26:33 +0500

    Add files via upload

---
+++ resturanth-w.js
@@ -0,0 +1,182 @@
+let kolachi = {
+    name: "Kolachi Resturant",
+    location: "D.H.A phase 6",
+    address: "Creak Side، Abdul Sattar Edhi Ave, D.H.A. Phase 8 Zone C Phase 8 Karachi, Sindh 75500",
+    hours: [
+        "Thursday	7pm–1am",
+        "Friday	7pm–1am",
+        "Saturday	7pm–1am",
+        "Sunday	7pm–1am",
+        "Monday	7pm–1am",
+        "Tuesday	7pm–1am",
+        "Wednesday	7pm–1am"
+    ],
+    phone: "(021) 111 111 001",
+    employees: [{
+            name: "Mr. Ahmed Khan",
+            salary: 10000,
+            age: 24,
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            qualification: "B.A",
+            designation: "waiter"
+
+        },
+        {
+            name: "saad sheikh",
+            fatherName: "zaid sheikh",
+            salary: 10000,
+            age: 22,
+            maritalStatus: "unmarried",
+            Gender: "Male",
+            qualification: "Intermediate",
+            designation: "waiter"
+        },
+        {
+            name: "dabeer khan",
+            fatherName: "sajjid",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 10000,
+            age: 23,
+            qualification: "Intermediate",
+            designation: "waiter"
+        },
+        {
+            name: "Umair sheikh",
+            fatherName: "Sheikh saeed",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 10000,
+            age: 25,
+            qualification: "Matriculation",
+            designation: "waiter"
+        },
+        {
+            name: "Bilal khan",
+            Gender: "Male",
+            fatherName: "umar khan",
+            salary: 10000,
+            maritalStatus: "unmarried",
+            age: 19,
+            qualification: "Matriculation",
+            designation: "waiter"
+        },
+        {
+            name: "Bismillah khan",
+            fatherName: "majeed khan",
+            salary: 10000,
+            maritalStatus: "unmarried",
+            age: 19,
+            Gender: "Male",
+            qualification: "Matriculation",
+            designation: "waiter"
+        },
+        {
+            name: "Abdullah",
+            fatherName: "khurram",
+            maritalStatus: "unmarried",
+            salary: 10000,
+            age: 21,
+            Gender: "Male",
+            qualification: "Matriculation",
+            designation: "waiter"
+        }
+
+    ],
+    cheifs: [
+        {
+            name: "Noman khan",
+            fatherName: "",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: "",
+            age: "",
+            qualification: "",
+            skills: "",
+        },
+        {
+            name: "",
+            Gender: "Male",
+            fatherName: "",
+            salary: "",
+            age: "",
+            maritalStatus: "unmarried",
+            qualification: "",
+            skills: "",
+        },
+        {
+            name: "",
+            fatherName: "",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: "",
+            age: "",
+            qualification: "",
+            skills: "",
+        },
+        {
+            name: "",
+            fatherName: "",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: "",
+            age: "",
+            qualification: "",
+            skills: "",
+        }
+    ],
+    branches: [{
+
+        },
+        {
+
+        },
+        {
+
+        },
+        {
+
+        }
+    ],
+    dishes: [{
+
+        },
+        {
+
+        },
+        {
+
+        },
+        {
+
+        },
+        {
+
+        },
+        {
+
+        },
+    ],
+    managers: [
+
+    ],
+    events: [
+
+    ],
+    reservations: [
+
+    ],
+    advantages: [
+
+    ],
+expenses: [
+
+],
+revenues: [
+
+],
+
+
+
+}
