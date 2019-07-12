9a47e3d162234e80e31f5fceb28223ad4be9e812
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-07-13 03:37:10 +0500

    Add files via upload

--- resturanth-w.js
+++ resturanth-w.js
@@ -12,15 +12,17 @@ let kolachi = {
         "Wednesday	7pm–1am"
     ],
     phone: "(021) 111 111 001",
-    employees: [{
+    employees: [
+        {
             name: "Mr. Ahmed Khan",
             salary: 10000,
             age: 24,
             Gender: "Male",
             maritalStatus: "unmarried",
             qualification: "B.A",
-            designation: "waiter"
-
+            designation: "waiter",
+            contact: "03490876541",
+            address: "sygiosfja"
         },
         {
             name: "saad sheikh",
@@ -30,7 +32,9 @@ let kolachi = {
             maritalStatus: "unmarried",
             Gender: "Male",
             qualification: "Intermediate",
-            designation: "waiter"
+            designation: "waiter",
+            contact: "04568907345",
+            address: "h#12,st#4,area karachi"
         },
         {
             name: "dabeer khan",
@@ -40,7 +44,9 @@ let kolachi = {
             salary: 10000,
             age: 23,
             qualification: "Intermediate",
-            designation: "waiter"
+            designation: "waiter",
+            contact: "09876543211",
+            address: "h#4,st#6,area karachi"
         },
         {
             name: "Umair sheikh",
@@ -50,7 +56,9 @@ let kolachi = {
             salary: 10000,
             age: 25,
             qualification: "Matriculation",
-            designation: "waiter"
+            contact: "03409876543",
+            designation: "waiter",
+            address: "h#12,st#4,area karachi"
         },
         {
             name: "Bilal khan",
@@ -60,7 +68,9 @@ let kolachi = {
             maritalStatus: "unmarried",
             age: 19,
             qualification: "Matriculation",
-            designation: "waiter"
+            designation: "waiter",
+            contact: "03769098090",
+            address: "h#4,st#6,area karachi"
         },
         {
             name: "Bismillah khan",
@@ -70,7 +80,9 @@ let kolachi = {
             age: 19,
             Gender: "Male",
             qualification: "Matriculation",
-            designation: "waiter"
+            designation: "waiter",
+            contact: "03456543211",
+            address: "karachi"
         },
         {
             name: "Abdullah",
@@ -80,103 +92,454 @@ let kolachi = {
             age: 21,
             Gender: "Male",
             qualification: "Matriculation",
-            designation: "waiter"
+            designation: "waiter",
+            contact: "0345987654",
+            address: "defence"
+        },
+        {
+            name: "mahmood",
+            fatherName: "iqrar",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 15000,
+            age: 27,
+            designation: "security guard",
+            qualification: "matric",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            designation: "security guard",
+            qualification: "Matric",
+            contact: "02476543211",
+            address: "Tin talwar"
+        },
+        {
+            name: "huzaifa khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 14500,
+            age: 27,
+            designation: "security guard",
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "landhi"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 27,
+            qualification: "Matric",
+            contact: "0239455211",
+            designation: "security guard",
+            address: "korangi"
+        },
+        {
+            name: "Noman khan",
+            fatherName: "Arshad",
+            Gender: "Male",
+            maritalStatus: "married",
+            salary: 40000,
+            age: 27,
+            qualification: "Matric",
+            contact: "03476543211",
+            address: "malir",
+            designation: "security guard"
         }
 
     ],
     cheifs: [
         {
             name: "Noman khan",
-            fatherName: "",
+            fatherName: "Arshad",
             Gender: "Male",
             maritalStatus: "unmarried",
-            salary: "",
-            age: "",
-            qualification: "",
-            skills: "",
+            salary: 40000,
+            age: 27,
+            qualification: "Masters in food science",
+            contact: "09876543211",
+            address: "Tin talwar"
         },
         {
-            name: "",
-            Gender: "Male",
-            fatherName: "",
-            salary: "",
-            age: "",
+            name: "anabia sheikh",
+            fatherName: "Asad sheikh",
+            salary: 20000,
+            age: 22,
+            Gender: "female",
             maritalStatus: "unmarried",
-            qualification: "",
-            skills: "",
+            qualification: "Masters",
+            contact: "09876543211",
+            address: "liyari"
         },
         {
-            name: "",
-            fatherName: "",
+            name: "Mohsin khan",
+            fatherName: "aziz khan",
             Gender: "Male",
-            maritalStatus: "unmarried",
-            salary: "",
-            age: "",
-            qualification: "",
-            skills: "",
+            maritalStatus: "married",
+            salary: 30000,
+            age: 30,
+            qualification: "Masters",
+            contact: "09876543211",
+            address: "Sadar"
         },
         {
-            name: "",
-            fatherName: "",
-            Gender: "Male",
-            maritalStatus: "unmarried",
-            salary: "",
-            age: "",
-            qualification: "",
-            skills: "",
+            name: "sidra akram",
+            fatherName: "akram",
+            Gender: "female",
+            maritalStatus: "married",
+            salary: 30000,
+            age: 28,
+            qualification: "Masters",
+            contact: "03976543211",
+            address: "Defence"
         }
     ],
-    branches: [{
-
-        },
-        {
-
-        },
-        {
-
-        },
-        {
-
-        }
-    ],
-    dishes: [{
-
-        },
-        {
-
-        },
-        {
-
-        },
-        {
-
-        },
-        {
-
-        },
-        {
-
-        },
-    ],
+    
     managers: [
+        {
+            name: "Mohsin khan",
+            fatherName: "aziz khan",
+            Gender: "Male",
+            maritalStatus: "married",
+            salary: 50000,
+            age: 30,
+            designation: "event manager",
+            qualification: "bachelors",
+            contact: "09876543211",
+            address: "Sadar"
+        },
+        {
+            name: "Irshad khan",
+            fatherName: "aziz khan",
+            Gender: "Male",
+            maritalStatus: "married",
+            salary: 55000,
+            age: 60,
+            designation: "reserve manager",
+            qualification: "Masters",
+            contact: "09876543211",
+            address: "karachi"
 
+        },
+        {
+            name: "ibad ul lah",
+            fatherName: "aman ullah",
+            Gender: "Male",
+            maritalStatus: "married",
+            salary: 50000,
+            age: 40,
+            qualification: "Masters",
+            contact: "09876543211",
+            designation: "kitchen manager",
+            address: "Liyari"
+        },
+        {
+
+            name: "moiz khan",
+            fatherName: "huzaifa khan",
+            Gender: "Male",
+            maritalStatus: "married",
+            salary: 60000,
+            age: 50,
+            qualification: "bachelors",
+            contact: "0390543211",
+            designation: "cooking manager",
+            address: "karachi"
+        },
+        {
+
+            name: "faizan",
+            fatherName: "imran",
+            Gender: "Male",
+            maritalStatus: "unmarried",
+            salary: 40000,
+            age: 23,
+            qualification: "Masters",
+            contact: "02006543211",
+            designation: "area manager",
+            address: "Defence"
+        },
+        {
+            name: "aiman khan",
+            fatherName: "aziz khan",
+            Gender: "female",
+            maritalStatus: "married",
+            salary: 55000,
+            age: 60,
+            designation: "reserve manager",
+            qualification: "Masters",
+            contact: "09876543211",
+            address: "karachi"
+        },
+        {
+            
+            name: "mahira ahmed",
+            fatherName: "ahmed",
+            Gender: "female",
+            maritalStatus: "married",
+            salary: 55000,
+            age: 60,
+            designation: "parties manager",
+            qualification: "Masters",
+            contact: "0333333211",
+            address: "karachi"
+        }
     ],
     events: [
-
+        {
+            name: "birthday party",
+            facilities: "we give beautiful places for party and dexoration for birthday",
+            cake: "we provide every type of cake"
+        },
+        {
+            name: "conference table",
+            facilities: "we give a separate place for confrerence where youi can make ur conference more effective"
+        },
+        {
+            name: "recepitons",
+            description: "klgi ayyfh9fdh heiyf gedrmk8yr dhirerbt wqewgbudisad"
+        },
+        {
+            name: "school functions",
+            description: "yyib uyr gb oaq9489b ibvw8e hu6vc"
+        },
+        {
+            name: "office parties",
+            description: "you can make your freinds happy after inviting them in beautiful place"
+        }
     ],
-    reservations: [
-
-    ],
+    reservations: "you can reserve your seat before coming due to shortage of seats",
     advantages: [
-
+        "place",
+        "seaside",
+        "peace",
+        "family place",
+        "receptions",
+        "party",
+        "Functions",
+        "ful security",
+        "play land",
+        "for every type of office meeting"
     ],
-expenses: [
+    expenses: [
+        "Electricity bill expenses",
+        "advertismnent expense",
+        "salaries expenses",
+        "Decoration expenses",
+        "Cooking exprenses"
+    ],
+    menu: [
+        "fish",
+        "chicken",
+        "beaf",
+        "mutton",
+        "malai boti",
+        "chiken tikka",
+        "chines food",
+        "roll",
+        "chicken roll",
+        "mayo roll",
+        "french fries",
+        "biryani",
+        "chicken biryani",
+        "beaf biryani",
+        "beaf botti",
+        "mutton biryani",
+        "mutton chops",
+        "pizza",
+        "pizza fries",
+        "gola kabab",
+        "palao",
+        "yakhni",
+        "soup",
+        "spaghetti",
+        "handi",
+        "cheese naan",
+        "dam qeema",
+        "macroni",
+        "chicken mancholian",
+        "chaomin",
+        "fajita",
+        "tikka boti",
+        "mandi",
+        "chargha",
+        "kaleji",
+        "prawn",
+        "sea foods",
+        "hot dogs",
+        "hot wings"
+    ],
+    fastFood: [
+        "zinger",
+        "burger",
+        "checken burger",
+        "beaf burger",
+        "broast",
+        "gol gappe",
+        "chart"
+    ],
+    deserts: [
+        "chocolate cake",
+        "kheer",
+        "custard",
+        "icecreams",
+        "sweets",
+        "vanila cake",
+        "chocolaty desert",
+        "strawberry flavour cake"
+    ],
+    drinks: [
+        "soft drinks",
+        "cold drinks",
+        "lemon juice",
+        "milk shakes",
+        "banana shakes",
+        "mango shakes",
+        "strawberry shakes",
+        "orange juice",
+        "pista  flavour juice",
+        "oreo shakes",
+        "chocolate shakes",
+        "tea",
+        "cofee",
+        "kahwa",
+        "water"
+    ],
+    dishesPrice: [
+        {
+            name: "malai boti",
+            price: 500
+        },
+        {
+            name: "tikka",
+            price: 1000
+        },
+        {
+            name: "Fish",
+            price: 800
+        },
+        {
+            name: "Gola kabab",
+            price: 500
+        },
+        {
+            name: "chicken tandori",
+            price: 1000
+        },
+        {
+            name: "biryani",
+            price: 200
 
-],
-revenues: [
-
-],
-
-
+        }
+    ],
 
 }
