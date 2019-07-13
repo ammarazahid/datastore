309cb5d8baa7348f78aa84e378060607b31811fe
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-07-13 11:03:11 +0500

    Add files via upload

---
+++ whatsapp-homework.js
@@ -0,0 +1,225 @@
+let whatsapp = {
+    appName: "whatsApp",
+    about: "for contact and fun",
+    description: "In this we can make a call,video call, we can send messages,we can upload pictures and status as well.",
+    howToUse: "For using this we need wifi connection or data connection",
+    advantages: [
+        "voice call",
+        "video call",
+        "messages",
+        "snaps",
+        "audio recording",
+        "fun",
+        "status",
+    ],
+    contacts: [
+        {
+            name: "Miss Abeera",
+            lastmessage: "Kal bt karen??",
+            time: "9:30 PM yesterday",
+            contact: "0340345698",
+            profilePic: url(images / dp1.png),
+            isUnread: "true",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Miss Khair-un-nisa",
+            lastmessage: "Miss",
+            time: "3:56 PM",
+            contact: "0322222298",
+            profilePic: url(images / dp2.png),
+            isUnread: "false",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Areeba khan",
+            lastmessage: "Hahahahahaah......",
+            time: "3:56 AM",
+            contact: "0356666688",
+            profilePic: url(images/dp1.png),
+            isUnread: "true",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Ramsha khan",
+            lastmessage: "Kal bt karen??",
+            time: "9:30 PM",
+            contact: "0340345698",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "esha",
+            lastmessage: "oye?",
+            time: "9:30 PM",
+            contact: "0340342544",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "false"
+        },
+        {
+            name: "amna",
+            lastmessage: "sun??",
+            time: "5: 30 pm today",
+            contact: "03403456454",
+            profilePic: url(images / dp4.png),
+            isUnread: "false",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Raiba",
+            lastmessage: "codding krli??",
+            time: "9:38 am yesterday",
+            contact: "034034777",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "bareerah",
+            lastmessage: "okay",
+            time: "6:40 PM todya",
+            contact: "0345555698",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "false"
+        },
+        {
+            name: "ilaya",
+            lastmessage: "thk hai",
+            time: "9:39 AM yeserday",
+            contact: "036958778",
+            profilePic: url(images / dp4.png),
+            isUnread: "false",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "miss sarah",
+            lastmessage: "very good",
+            time: "10:14 PM yesterday",
+            contact: "03408796541",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "false"
+        },
+        {
+            name: "Maheen",
+            lastmessage: "achaw",
+            time: "11:03 PM today",
+            contact: "0349846325",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "false"
+        },
+        {
+            name: "nida",
+            lastmessage: "Questions solve krliye",
+            time: "4: 15 pm yesterday",
+            contact: "0349872398",
+            profilePic: url(images / dp4.png),
+            isUnread: "false",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Akashooo00",
+            lastmessage: "HAhah thk thk",
+            time: "6:06 PM today",
+            contact: "03409874626",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Muqqi",
+            lastmessage: ":(",
+            time: "9: 50 am today",
+            contact: "03442271237",
+            profilePic: url(images / dp4.png),
+            isUnread: "false",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "MAm shahla",
+            lastmessage: "Ammara???",
+            time: "6:10 AM today",
+            contact: "03122554489",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "false"
+        },
+        {
+            name: "Tom",
+            lastmessage: ";)",
+            time: "4:14 AM today",
+            contact: "03421125577",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "false"
+        },
+        {
+            name: "doremon",
+            lastmessage: ":)",
+            time: "&:40 pm yesterday",
+            contact: "03112244556",
+            profilePic: url(images / dp4.png),
+            isUnread: "false",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Pikachu",
+            lastmessage: "hello!!",
+            time: "8:12 AM today",
+            contact: "0340345698",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "noreen",
+            lastmessage: "humam",
+            time: "10:58 pm yesterday",
+            contact: "0340345698",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Arfa",
+            lastmessage: "kb aogi",
+            time: "9:00 PM yesterday",
+            contact: "0340345698",
+            profilePic: url(images / dp4.png),
+            isUnread: "false",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Behzad",
+            lastmessage: "Hahahah......",
+            time: "9:13 Am yesterday",
+            contact: "031888698",
+            profilePic: url(images / dp4.png),
+            isUnread: "true",
+            isStatusUploaded : "false"
+        },
+        {
+            name: "Mom",
+            lastmessage: "Khan ho",
+            time: "9:30 PM today",
+            contact: "0342654558",
+            profilePic: url(images / dp4.png),
+            isUnread: "false",
+            isStatusUploaded : "true"
+        },
+        {
+            name: "Phoppo",
+            lastmessage: "sahi",
+            time: "9:40 AM yesterday",
+            contact: "0340987456",
+            profilePic: url(images / dp4.png),
+            isUnread: "false",
+            isStatusUploaded : "true"
+        }
+
+    ]
+}
