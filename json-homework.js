37bd2c43c688fd6448905c1be747a4232b002aab
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-07-15 22:37:02 +0500

    Add files via upload

---
+++ json-homework.js
@@ -0,0 +1,92 @@
+let world = {
+    name: "world",
+    meanings: "dunya",
+    description: "dunya word is derived from urdu word dana thats mean is .World is divided into two parts land and water.",
+    division: [
+        {
+            part1: "land",
+            description: "1/3 part is land",
+            divisionOfLAnd: [
+                {
+                    Continents: [
+                        {
+                            continent: "Africa",
+                        },
+                        {
+                            continent: "Antarctica",
+                        },
+                        {
+                            continent: "Asia",
+                            description: "Asia is the biggest in the whole world"
+                        },
+                        {
+                            continent: "Australia"
+                        },
+                        {
+                            continent: "Europe"
+                        },
+                        {
+                            continent: "North",
+                        },
+                        {
+                            continent: "America"
+                        },
+                        {
+                            continent: "South"
+                        },
+                    ],
+                }
+            ]
+        },
+        {
+            part2: "water",
+            description: "2/3 part is water",
+            advantage: [
+                {
+                    one: "water is a great blessing of almighty Allah and is an important need of human beings"
+                },
+                {
+            two: "water maintance 70% of the total weight of human body"
+                }
+            ],
+            Oceans: [
+                {
+                    Ocean: "Arctic Ocean",
+                    description: "The Arctic Ocean is the smallest and shallowest of the world's five major ocean"
+                },
+                {
+                    Ocean: "Atlantic Ocean",
+                    description: "The Atlantic Ocean is the second largest of the world's oceans, with an area of about 106,460,000 square kilometers (41,100,000 square miles)."
+                },
+                {
+                    Ocean: "Indian Ocean",
+                    description: "The Indian Ocean is the third-largest of the world's oceanic divisions, covering 70,560,000 km2 (27,240,000 sq mi) (19.8% of the water on the Earth's surface)."
+        
+                },
+                {
+                    Ocean: "Pacific Ocean",
+                    description: "The Pacific Ocean is the largest and deepest of Earth's oceanic divisions"
+                },
+                {
+                    Ocean: "Southern Ocean",
+                    description: "The Southern Ocean, also known as the Antarctic Ocean[1] or the Austral Ocean,[2][note 4] comprises the southernmost waters of the World Ocean, generally taken to be south of 60° S latitude and encircling Antarctica."
+
+                }
+            ],
+            waterDivision: [
+                {
+                    first: "Salty water",
+                    percentage: "97.2%",
+                    description: "97.2% water is salty and nmot suitable for drinking and for other use."
+                },
+                {
+                    second: "potable Water",
+                    percentage: "2.8%",
+                    description: "0.65% of this water is in liquid form and the rest is in the form of snow and glaciers"
+                }
+            ]
+        }
+    ],
+    planet: "The world is the planet Earth and all life on it, including human civilization",
+ 
+}
---
+++ techkaro.js
@@ -0,0 +1,216 @@
+let techkaro = {
+    name: "Tech Karo",
+    about: "A Web Dev Course",
+    purpose: "after getting knowledge about web developing every girl and women will be able to built our economy.and pakistan will be a developed country.",  
+    description: "This program is held for womens.atfer this program every women and girl will change her life every girl can do every thing.Now female loves tech.",
+    noOFLabs: 3,
+    noOfConputers: 100,
+    sponseredBy: "Engro Vopak Terminal",
+    fees :{
+        perMonth: 500
+    },
+    students: [
+        {
+            name: "Ammara",
+            age: 19,
+            email: "ammarazahid845@gmail.com",
+            cnic: 4021 - 3457 - 23,
+            group: "B",
+            gender: "Female",
+            address: "Korangi,karachi",
+            contact: "03408589699"
+        },
+        {
+            name: "Abeera",
+            age: 20,
+            email: "abeera2900@gmail.com",
+            group: "B",
+            cnic: 4032 - 45678 - 2,
+            gender: "Female",
+            address: "karachi",
+            contact: "0346807587"
+        }
+    ],
+    scholarship: [
+        {
+            monthlyScholarShip: "If someone can not pay his fee any month,so techkaro gives them scholarship of that month."
+        },
+        {
+            fullScholarship: "if someone can not his fees of this course so techkaro gives them scholarship.",
+        }
+    ],
+    events: [
+        "oreintation",
+        "eid party"
+    ],
+    timmings: [
+        {
+            day: "tuesday",
+            time: "2 PM to 6 PM"
+        },
+        {
+            day: "thursday",
+            time: "2Pm to 6PM"
+        },
+        {
+            day: "saturday",
+            time: "2PM to 6PM"
+        }
+    ],
+    days: [
+        "saturday",
+        "tuesday",
+        "thursday"
+    ],
+    teacher: {
+        name: "Miss Sarah Ahmed",
+        age: "22",
+        qualfication: "bscs from NED university",
+        description: "A full stack web developer",
+        contact: "0234687807",
+        email: "sarah0988@gmail.com",
+        address: "yghodub",
+        experience: "4 years"
+    },
+    founder: {
+        name: "Miss Sadaf Abid",
+        contact: "90273864552",
+        email: "sadafabid087@gmail.com",
+        aim: "she wants every girl learn tech and do codding"
+    },
+    manager: {
+        name: "tuba Mohsin",
+        contact: "046836897875",
+        qualfication: "bscs from NED university",
+        email: "tubamohsin@gmail.com",
+    },
+    mentors: [
+        "maryam",
+        "nazia",
+        "amna",
+        "saad",
+        "shehryar"
+    ],
+    ambasadors: [
+        "KhairunNisa",
+        "Ali Khan",
+        "Abeera Tariq",
+        "arsala",
+        "noreen naz",
+        "wali",
+        "waqar",
+        "rabia"
+    ],
+    address: [
+        {
+            place: "Usman Institute of technology"
+        },
+        {
+            Area: "Gulshan"
+        },
+        {
+            city: "karachi"
+        },
+        {
+            country: "pakistan"
+        }
+    ],
+    syllabus: [
+        "html",
+        "css",
+        "Javascript",
+        "wordpress",
+        "react"
+    ],
+    projects: [
+        "weather App",
+        "Calculator",
+        "Fruit shop",
+        "Quiz task"
+    ],
+    supporters: [
+        {
+            name: "usman institute of technology",
+            description: "UIT provides us computer labs for students and also provides lecture halls for life skils."
+        },
+        {
+            organizationName : "careem",
+            description: "careem provides and supports techkaro students 50% discount on every type of services"
+
+        }
+],
+    lifeSkills: [
+        "cv writting",
+        "body Language",
+        "behaviour",
+        "attitude shift",
+        "Presentations",
+        "vision",
+        "grouping"
+    ],
+    careemPromocode: "careem gives 50% promocode for techkaro students.",
+    heardFrom: [
+        "The citizen foundation",
+        "Zindagi trust",
+    ],
+    advisor: "mashhood rasger",
+    mentorsMeetup: "mentors come for supporting  and sharing their problems and succeed stories for students.",
+    photoshop: [
+        {
+            name: "UI design & UX design",
+            description: "techkaro gives opportunity to their student through providing photoshop's classes.",
+        }
+    ],
+    bestproject: [
+        {
+            name: "Alaina",
+            age: 22,
+            qualification: "master's",
+            contact: "0273975897",
+            address: "malir",
+            skills: [
+                "codding",
+                "punctaulity",
+                "regularity",
+                "leadership"
+            ],
+            email: "alaina234@gmail.com"
+        },
+        {
+            name: "Usaid",
+            age: 20,
+            qualification: "Intermediate",
+            contact: "034567890",
+            address: "Defence",
+            skills: [
+                "codding",
+                "punctaulity",
+                "regularity",
+                "groupleading",
+                "management"
+            ],
+            email: "usaidk678@yahoo.com"   
+        }
+    ],
+    test: [
+        {
+           topic: "html",
+           type: "quiz test",
+           timings: "half hour"
+        },
+        {
+            topic: "css",
+            type: "quiz test",
+            timings: "half hour"
+        },
+        {
+            topic: "css",
+            type: "css buttons",
+            timings: "2 hours"
+        }
+    ],
+    hackaton: {
+        name: "slack task",
+        timings: "2 haours"  
+    },
+}
